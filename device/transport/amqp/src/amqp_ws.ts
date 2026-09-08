e5437eda0136079b410cc550db950739ef320b3d
nkaaf <nkaaf@protonmail.com>
2026-09-09 01:01:20 +0200

    fix[device-transport-amqp]: Fix JSDoc error for TypeScript 7 support

--- device/transport/amqp/src/amqp_ws.ts
+++ device/transport/amqp/src/amqp_ws.ts
@@ -10,7 +10,7 @@ import { AuthenticationProvider, TransportConfig } from 'azure-iot-common';
  * Constructs a transport object that can be used by the device {@link azure-iot-device.Client} to send and receive messages to and from an IoT Hub instance, using the AMQP protocol over secure websockets.
  * This class overloads the constructor of the base {@link azure-iot-device-amqp.Amqp} class from the AMQP transport, and inherits all methods from it.
  *
- * @augments module:azure-iot-device-amqp.Amqp
+ * @augments Amqp
  */
 export class AmqpWs extends Amqp {
   /**
